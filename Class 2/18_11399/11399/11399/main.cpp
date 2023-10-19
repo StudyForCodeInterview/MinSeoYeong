d5df979353898e64019f04637fdbd74b995242c9
seoyyeong <minsssyyyyy@gmail.com>
2023-10-20 00:33:22 +0900

    Class 2

---
+++ Class 2/18_11399/11399/11399/main.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	vector<int> arr;
+	int n;
+	int temp;
+	int ans = 0;
+
+	scanf("%d", &n);
+	arr.push_back(0);
+
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d", &temp);
+		arr.push_back(temp);
+	}
+
+	sort(arr.begin(), arr.end());
+
+	for (int i = 1; i <= n; i++)
+	{
+		arr[i] += arr[i - 1];
+		ans += arr[i];
+	}
+
+	printf("%d", ans);
+
+	return 0;
+}
