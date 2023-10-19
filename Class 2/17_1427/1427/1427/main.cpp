022d2de6b7531e2d5400c58ba1ab3327db94f304
seoyyeong <minsssyyyyy@gmail.com>
2023-10-20 00:19:20 +0900

    Class 2

---
+++ Class 2/17_1427/1427/1427/main.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+int cmp(const void* a, const void* b);
+
+int main(void)
+{
+	char arr[11];
+	scanf("%s", arr);
+	qsort(arr, strlen(arr), sizeof(char), cmp);
+	printf("%s", arr);
+
+}
+
+int cmp(const void* a, const void* b)
+{
+	return *(char*)b - *(char*)a;
+}
