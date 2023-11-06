2ab8146ad9c85c07ee2a338a7efacf987cf13bd3
seoyyeong <minsssyyyyy@gmail.com>
2023-11-07 01:47:55 +0900

    Class 3

---
+++ Class 3/49_2251__/2251/2251/main.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+
+using namespace std;
+
+queue<int> q;
+set<int> ans;
+int a;
+int b;
+int c;
+
+int main(void)
+{
+	scanf("%d %d %d", &a, &b, &c);
+
+	
+
+
+
+	return 0;
+}
